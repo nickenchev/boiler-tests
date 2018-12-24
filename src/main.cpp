47fba75c93cc67fa6137bb52986354303dadc5b0
Nick Enchev <nikolaenchev@gmail.com>
2018-12-24 11:51:04 -0500

    Added rect tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,35 @@
+#define CATCH_CONFIG_MAIN
 #include <catch.hpp>
 #include <boiler.h>
 
-int main(int argc, char *argv[])
+TEST_CASE("Size initialization", "[Size]")
 {
-    return 0;
+	Size size(50, 90);
+
+	REQUIRE(size.width == 50);
+	REQUIRE(size.height == 90);
+}
+
+TEST_CASE("Rect initialization and min/max methods", "[Rect]")
+{
+	Rect rect(10, 20, Size(25, 45));
+
+	SECTION("Initiailize Rect and ensure members are set correctly")
+	{
+		REQUIRE(rect.position.x == 10);
+		REQUIRE(rect.position.y == 20);
+		REQUIRE(rect.size.width == 25);
+		REQUIRE(rect.size.height == 45);
+	}
+
+	SECTION("Min/max computation")
+	{
+		REQUIRE(rect.getMinX() == 10);
+		REQUIRE(rect.getMinY() == 20);
+		REQUIRE(rect.getMaxX() == 35);
+		REQUIRE(rect.getMaxY() == 65);
+
+		rect.position.y = 43;
+		REQUIRE(rect.getMaxY() == 88);
+	}
 }
